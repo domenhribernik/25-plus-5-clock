8d9ccae885047e127a97f89751e4189751f4720d
domenhribernik <domen.hribernik4@gmail.com>
2020-11-07 18:36:06 +0100

    trying to fix 00:00 bug

--- src/App.js
+++ src/App.js
@@ -8,107 +8,136 @@ class App extends Component {
     this.state = {
       breakLength: 5,
       sessionLength: 25,
-      minutes: 25,
+      minutes: "25",
       seconds: "00",
-      interval: ""
+      interval: 0,
+      currentInterval: "Session",
+      startStyle: {display: "inline-block"},
+      stopStyle: {display: "none"}
     }
     this.handleIncrement = this.handleIncrement.bind(this)
     this.handleDecrement = this.handleDecrement.bind(this)
     this.handleReset = this.handleReset.bind(this)
-    this.handleStart = this.handleStart.bind(this)
-    this.handleStop = this.handleStop.bind(this)
+    this.handleStartStop = this.handleStartStop.bind(this)
     this.timer = this.timer.bind(this)
   }
   handleIncrement(e) {
     e.preventDefault()
-    // document.getElementById("reset").parentElement
     if (e.target.parentElement.id.includes("break") && this.state.breakLength < 60) {
-      this.setState(prevState => ({
-        breakLength: prevState.breakLength + 1
-      }))
+      if (this.state.currentInterval === "Break") {
+        this.setState(prevState => ({
+          breakLength: prevState.breakLength + 1,
+          minutes: parseInt(prevState.breakLength) + 1 < 10 ? "0" + (parseInt(prevState.breakLength) + 1).toString() : (parseInt(prevState.breakLength) + 1).toString()
+        }))
+      } else {
+        this.setState(prevState => ({
+          breakLength: prevState.breakLength + 1
+        }))
+      }
     } else if (e.target.parentElement.id.includes("session") && this.state.sessionLength < 60) {
-      this.setState(prevState => ({
-        sessionLength: prevState.sessionLength + 1,
-        minutes: prevState.sessionLength + 1
-      })) 
-    }
-    if (this.state.minutes >= 9) {
-      console.log(this.state.minutes);
-      this.setState(prevState => ({
-        minutes: prevState.minutes
-      }))
-    } else if (this.state.minutes <= 10) {
-      console.log(this.state.minutes);
-      this.setState(prevState => ({
-        minutes: "0" + prevState.minutes
-      }))
+      if (this.state.currentInterval === "Session") {
+        this.setState(prevState => ({
+          sessionLength: prevState.sessionLength + 1,
+          minutes: parseInt(prevState.sessionLength) + 1 < 10 ? "0" + (parseInt(prevState.sessionLength) + 1).toString() : (parseInt(prevState.sessionLength) + 1).toString()
+        })) 
+      } else {
+        this.setState(prevState => ({
+          sessionLength: prevState.sessionLength + 1,
+        })) 
+      }
     }
   }
   handleDecrement(e) {
     e.preventDefault()
     if (e.target.parentElement.id.includes("break") && this.state.breakLength > 1) {
-      this.setState(prevState => ({
-        breakLength: prevState.breakLength - 1
-      }))
-    } else if (e.target.parentElement.id.includes("session") && this.state.sessionLength > 1 && this.state) {
-      this.setState(prevState => ({
-        sessionLength: prevState.sessionLength - 1,
-        minutes: prevState.sessionLength - 1
-      })) 
-    }
-    if (this.state.minutes <= 10) {
-      console.log(this.state.minutes);
-      this.setState(prevState => ({
-        minutes: "0" + prevState.minutes
-      }))
+      if (this.state.currentInterval === "Break") {
+        this.setState(prevState => ({
+          breakLength: prevState.breakLength - 1,
+          minutes: prevState.breakLength - 1 < 10 ? "0" + (prevState.breakLength - 1) : prevState.breakLength - 1
+        })) 
+      } else {
+        this.setState(prevState => ({
+          breakLength: prevState.breakLength - 1
+        }))
+      }
+    } else if (e.target.parentElement.id.includes("session") && this.state.sessionLength > 1) {
+      if (this.state.currentInterval === "Session") {
+        this.setState(prevState => ({
+          sessionLength: prevState.sessionLength - 1,
+          minutes: prevState.sessionLength - 1 < 10 ? "0" + (prevState.sessionLength - 1) : prevState.sessionLength - 1
+        })) 
+      } else {
+        this.setState(prevState => ({
+          sessionLength: prevState.sessionLength - 1,
+        })) 
+      }
     }
   }
+
   handleReset(e) {
     e.preventDefault()
+    clearInterval(this.state.interval)
+    document.getElementById("beep").pause()
     this.setState({
+      currentInterval: "Session",
       breakLength: 5,
       sessionLength: 25,
-      minutes: 25,
-      seconds: "00"
+      minutes: "25",
+      seconds: "00",
+      startStyle: {display: "inline-block"},
+      stopStyle: {display: "none"}
     })
   }
 
-  handleStart() {
+  clearInterval() {
+    clearInterval(this.state.interval)
     this.setState({
-      interval: setInterval(this.timer, 100)
+      interval: 0,
+      startStyle: {display: "inline-block"},
+      stopStyle: {display: "none"}
     })
+  }
+
+  handleStartStop() {
+    if (this.state.interval === 0) {
+      this.setState({
+        interval: setInterval(this.timer, 1000),
+        stopStyle: {display: "inline-block"},
+        startStyle: {display: "none"}
+      })
+    } else {
+      this.clearInterval()
+    }
   }
 
   timer() {
-    if (this.state.seconds === "00" && this.state.minutes > 0) {
+    if (this.state.seconds === "00" && this.state.minutes === "00") {
+      var sound = document.getElementById("beep")
+      sound.currentTime = 0
+      sound.play()
+      if (this.state.currentInterval === "Session") {
+        this.setState(prevState => ({
+          minutes: prevState.breakLength,
+          currentInterval: "Break"
+        }))
+        
+      } else if (this.state.currentInterval === "Break") {
+        this.setState(prevState => ({
+          minutes: prevState.sessionLength,
+          currentInterval: "Session"
+        }))
+      }
+    } else if (this.state.seconds === "00" && this.state.minutes > 0) {
       this.setState(prevState => ({
-        seconds: 59,
-        minutes: prevState.minutes - 1
+        seconds: "59",
+        minutes: prevState.minutes <= 10 ? "0" + (prevState.minutes - 1) : prevState.minutes - 1
       }))
-      
     } else if (this.state.seconds > 0) {
       this.setState(prevState => ({
-        seconds: prevState.seconds-1,
-        minutes: prevState.minutes+1-1
-      }))
-      
-    }
-    if (this.state.seconds < 10) {
-      console.log(this.state.seconds);
-      this.setState(prevState => ({
-        seconds: "0" + prevState.seconds
+        seconds: prevState.seconds <= 10 ? "0" + (prevState.seconds - 1) : prevState.seconds - 1,
+        minutes: prevState.minutes.toString().length === 1 ? "0" + prevState.minutes : prevState.minutes
       }))
     }
-    if (this.state.minutes <= 10) {
-      console.log(this.state.minutes);
-      this.setState(prevState => ({
-        minutes: "0" + prevState.minutes
-      }))
-    }
-  }
-
-  handleStop() {
-    clearInterval(this.state.interval)
   }
 
   render() {
@@ -118,16 +147,19 @@ class App extends Component {
           breakLength={this.state.breakLength}
           sessionLength={this.state.sessionLength}
           minutes={this.state.minutes}
-          seconds={this.state.seconds}        
+          seconds={this.state.seconds}   
+          currentInterval={this.state.currentInterval}     
+          startStyle={this.state.startStyle}
+          stopStyle={this.state.stopStyle}
           handleIncrement={this.handleIncrement}    
           handleDecrement={this.handleDecrement}        
           handleReset={this.handleReset}    
-          handleStart={this.handleStart}
-          handleStop={this.handleStop}
+          handleStartStop={this.handleStartStop}
         />
+        <audio id="beep" src="https://raw.githubusercontent.com/freeCodeCamp/cdn/master/build/testable-projects-fcc/audio/BeepSound.wav" preload="auto" />
       </div>
     )
   }
 }
 
-export default App;
+export default App
--- src/Timer.js
+++ src/Timer.js
@@ -7,25 +7,25 @@ class Timer extends Component {
         <div id="settings">
           <div id="break">
             <div id="break-label">Break Length</div>
-            <div id="break-increment" onClick={this.props.handleIncrement}><i className="fa fa-arrow-up"></i></div>
-            <div id="break-decrement" onClick={this.props.handleDecrement}><i className="fa fa-arrow-down"></i></div>
+            <div id="break-increment" onClick={this.props.handleIncrement}><i className="fa fa-plus"></i></div>
+            <div id="break-decrement" onClick={this.props.handleDecrement}><i className="fa fa-minus"></i></div>
             <div id="break-length">{this.props.breakLength}</div>
           </div>
           <div id="session">
             <div id="session-label">Session Length</div>
-            <div id="session-increment" onClick={this.props.handleIncrement}><i className="fa fa-arrow-up"></i></div>
-            <div id="session-decrement" onClick={this.props.handleDecrement}><i className="fa fa-arrow-down"></i></div>
+            <div id="session-increment" onClick={this.props.handleIncrement}><i className="fa fa-plus"></i></div>
+            <div id="session-decrement" onClick={this.props.handleDecrement}><i className="fa fa-minus"></i></div>
             <div id="session-length">{this.props.sessionLength}</div>
           </div>
         </div>
         <div id="time">
-          <div id="timer-label">Session</div>
+          <div id="timer-label">{this.props.currentInterval}</div>
           <div id="time-left">{`${this.props.minutes}:${this.props.seconds}`}</div>
         </div>
         <div id="buttons">
-          <div id="start_stop">
-          <div id="start" onClick={this.props.handleStart}><i className="fa fa-play"></i></div>
-          <div id="stop" onClick={this.props.handleStop}><i className="fa fa-pause"></i></div>
+          <div id="start_stop" onClick={this.props.handleStartStop}>
+            <div id="start" style={this.props.startStyle}><i className="fa fa-play"></i></div>
+            <div id="stop" style={this.props.stopStyle}><i className="fa fa-pause"></i></div>
           </div>
           <div id="reset" onClick={this.props.handleReset}><i className="fa fa-refresh"></i></div>
         </div>
